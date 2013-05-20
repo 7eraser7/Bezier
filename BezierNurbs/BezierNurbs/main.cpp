a1b9a01eeeee8e3ac3823484d5e19982ef6fcc5b
U-Antoine-TOSH\Antoine <Antoine@Antoine-PC.(none)>
2013-05-20 18:19:17 +0200

    ça marche a peu près

--- BezierNurbs/BezierNurbs/main.cpp
+++ BezierNurbs/BezierNurbs/main.cpp
@@ -6,11 +6,55 @@
 
 using namespace std;
 
+vector<vertex> bezierControlPoints;
 
+int windowHeight;
+int windowWidth;
+
+bool shouldShowEdges;
+
+void drawPoint(const vertex & v){
+	glBegin(GL_POINT);
+	glVertex2i(v.x, v.y);
+	glEnd();
+}
+
+void drawPoints(const vector<vertex> & points){
+	if (points.size() < 1)
+		return;
+
+	glBegin(GL_POINTS);
+	glPointSize(5.0f);
+	for (unsigned int i=0; i<points.size(); i++){
+		glVertex2i(points[i].x, points[i].y);
+	}
+	glEnd();
+}
+
+void drawSegments(const vector<vertex> & points){
+	if (points.size() < 2)
+		return;
+
+	glBegin(GL_LINES);
+	for (unsigned int i = 1; i<points.size(); i++){
+		glVertex2i(points[i-1].x, points[i-1].y);
+		glVertex2i(points[i].x, points[i].y);
+	}
+	glEnd();
+}
+
+void resetContext(){
+	bezierControlPoints = vector<vertex>();
+}
 
 void affichage(){
 
     glClear(GL_COLOR_BUFFER_BIT);
+
+	drawPoints(bezierControlPoints);
+
+	if (shouldShowEdges)
+		drawSegments(bezierControlPoints);
 
     // On force l'affichage du résultat
     glFlush();
@@ -21,6 +65,7 @@ void mouse(int button,int state,int x,int y)
     // Si on appuie sur le bouton de gauche
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
     {
+		bezierControlPoints.push_back(vertex(x, windowWidth - y));
 
         affichage();
     }
@@ -30,6 +75,14 @@ void mouse(int button,int state,int x,int y)
 /* Evènement du clavier */
 void clavier(unsigned char touche,int x,int y){
     switch (touche){
+	case 's':
+		shouldShowEdges = !shouldShowEdges;
+		affichage();
+		break;
+	case 'r':
+		resetContext();
+		affichage();
+		break;
     case 'q':/* Quitter le programme */
         exit(0);
     }
@@ -39,6 +92,7 @@ void afficherInformations(){
     cout<<"\n\n\n\n\n";
     cout<<"Liste des commandes :"<<endl;
     cout<<"Creez des vertex avec le clique gauche"<<endl;
+	cout<<"Appuyez sur 'S' pour montrer/cacher les segments"<<endl;
     cout<<"Appuyez sur 'R' pour reinitialiser"<<endl;
 	cout<<"Appuyez sur 'Q' pour quitter"<<endl;
 }
@@ -46,25 +100,29 @@ void afficherInformations(){
 // Main :
 
 int main(int argc, char **argv){
+	windowHeight = 800;
+	windowWidth = 600;
 
-	vector<vertex> bezierControlPoints = vector<vertex>();
+	shouldShowEdges = false;
+
+	bezierControlPoints = vector<vertex>();
 
     afficherInformations();
 
     /* Initialisation de glut et creation de la fenetre */
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB);
-    glutInitWindowSize(500, 500);
-    glutInitWindowPosition (100, 100);
+	glutInitWindowSize(windowHeight, windowWidth);
+    glutInitWindowPosition (0, 0);
     glutCreateWindow("Courbes de Bezier");
 
     /* Repère 2D délimitant les abscisses et les ordonnées*/
-    gluOrtho2D(-250.0,250.0,-250.0,250.0);
+    gluOrtho2D(0, windowHeight, 0, windowWidth);
 
     /* Initialisation d'OpenGL */
-    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     glColor3f(1.0f, 1.0f, 1.0f);
-    glPointSize(2.0);
+    glPointSize(2.0f);
 
     /* Enregistrement des fonctions de rappel */
     glutDisplayFunc(affichage);
